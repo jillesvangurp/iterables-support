f9c0d3600edb3b1048db3101a1678a50c4827cf6
Jilles van Gurp <jilles.vangurp@nokia.com>
2012-10-30 10:57:59 +0100

    add non concurrent processing iterable

---
+++ src/main/java/com/jillesvangurp/iterables/ProcessingIterable.java
@@ -0,0 +1,34 @@
+package com.jillesvangurp.iterables;
+
+import java.util.Iterator;
+
+public class ProcessingIterable <Input,Output> implements Iterable<Output> {
+
+    private final Iterator<Input> it;
+    private final Processor<Input, Output> processor;
+
+    public ProcessingIterable(Iterator<Input> it, Processor<Input, Output> processor) {
+        this.it = it;
+        this.processor = processor;
+    }
+    
+    @Override
+    public Iterator<Output> iterator() {
+        return new Iterator<Output>() {
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public Output next() {
+                return processor.process(it.next());
+            }
+
+            @Override
+            public void remove() {
+                it.remove();
+            }};
+    }
+}
